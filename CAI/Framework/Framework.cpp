c323129eb4e30c40e5117d573435f17beeacde50
Tomer Kigel <88850793+TomerKigel@users.noreply.github.com>
2023-07-14 17:12:42 +0300

    Update Framework.cpp

--- CAI/Framework/Framework.cpp
+++ CAI/Framework/Framework.cpp
@@ -1,12 +1,12 @@
 #include "Framework.h"
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/basic_file_sink.h"
+//#include "spdlog/spdlog.h"
+//#include "spdlog/sinks/basic_file_sink.h"
 Framework::Framework()
 {
-	auto sharedFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt");
-	auto framework_logger = std::make_shared<spdlog::logger>("main_logger", sharedFileSink);
-	spdlog::set_default_logger(framework_logger);
-	spdlog::info("Framework initializing");
+	//auto sharedFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt");
+	//auto framework_logger = std::make_shared<spdlog::logger>("main_logger", sharedFileSink);
+	//spdlog::set_default_logger(framework_logger);
+	//spdlog::info("Framework initializing");
     host_ = "127.0.0.1";
 	port_ = 7777;
 	end_point_ = boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(host_), port_);
@@ -17,7 +17,7 @@ Framework::Framework()
 	std::initializer_list<Component<message::Message>*> msg_based = { &*base_server_, &interpreter_ } ;
 	std::initializer_list<Component<message::Parsed_Message>*> pmsg_based = { &agent_network_, &engine_ };
 	SystemMediator_ = std::make_unique<Concrete_Mediator>(msg_based, pmsg_based);
-	spdlog::info("Framework initialized successfully");
+	//spdlog::info("Framework initialized successfully");
 }
 
 void Framework::start()
@@ -25,14 +25,14 @@ void Framework::start()
 	context_thread_ = std::thread([this]() { io_context_.run(); });
 	interpreter_thread_ = std::thread([this]() { interpreter_.run(); });
 	representational_network_thread_ = std::thread([this]() { agent_network_.run(); });
-	spdlog::info("Framework running");
+	//spdlog::info("Framework running");
 	engine_.run();
 	
 }
 
 void Framework::halt()
 {
-	spdlog::info("Framework halted");
+	//spdlog::info("Framework halted");
 }
 
 void Framework::close()
@@ -40,7 +40,7 @@ void Framework::close()
 	interpreter_.kill();
 	engine_.quit();
 	base_server_->close();
-	spdlog::info("Framework terminated");
+	//spdlog::info("Framework terminated");
 }
 
 Framework::~Framework()
