cb09d861412b438c46da3896672a7713068193e6
Tomer <tomer200000@gmail.com>
2023-07-15 22:30:20 +0300

    fix project settings and push unit tests

--- CAI_UnitTest/CAI_UnitTest.cpp
+++ CAI_UnitTest/CAI_UnitTest.cpp
@@ -8,11 +8,42 @@ namespace CAIUnitTest
 	TEST_CLASS(classtest)
 	{
 	public:
-		
-		TEST_METHOD(argamount)
+
+		TEST_METHOD(Message__Creation)
 		{
-			//task new_task();
-			//int x = new_task;
+			message::Message msg;
+			Assert::AreEqual(true, msg.direction == msg.In);
+			Assert::AreEqual(true, msg.body.size() == 0);
+			Assert::AreEqual(true, msg.header.body_size == 0);
+			Assert::AreEqual(true, msg.header.connection_id == 0);
+			Assert::AreEqual(true, msg.header.type.size() == 0);
+		}
+
+		TEST_METHOD(Message__to_string)
+		{
+			message::Message msg;
+			/*message::Message msg;
+			Message_Generator message_generator;
+			message::Parsed_Message pmsg;
+			Queue_Manager<message::Message> queue_manager;*/
+			//Assert::AreEqual(x, 2);
+		}
+
+		TEST_METHOD(x)
+		{
+			/*message::Message msg;
+			Message_Generator message_generator;
+			message::Parsed_Message pmsg;
+			Queue_Manager<message::Message> queue_manager;*/
+			//Assert::AreEqual(x, 2);
+		}
+
+		TEST_METHOD(y)
+		{
+			/*message::Message msg;
+			Message_Generator message_generator;
+			message::Parsed_Message pmsg;
+			Queue_Manager<message::Message> queue_manager;*/
 			//Assert::AreEqual(x, 2);
 		}
 	};
