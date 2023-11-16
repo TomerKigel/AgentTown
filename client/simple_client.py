234c63282511de2b4ab21213bc8e8cc6e7a3c6f7
Tomer Kigel <88850793+TomerKigel@users.noreply.github.com>
2023-11-16 17:28:59 +0300

    Update simple_client.py

--- client/simple_client.py
+++ client/simple_client.py
@@ -1,21 +1,40 @@
 # Import socket module
 import socket
 import time
+from random import random
 from threading import Thread
+def client_run(id):
+    # Create a socket object
+    s = socket.socket()
 
-# Create a socket object
-s = socket.socket()
+    # Define the port on which you want to connect
+    port = 7777
 
-# Define the port on which you want to connect
-port = 7777
+    # connect to the server on local computer
+    s.connect(('127.0.0.1', port))
 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
+    x = int(random() * 1000)
+    y = int(random() * 1000)
 
-# receive data from the server and decoding to get the string.
-s.send(bytes("[new id]{new_id:1\nx:100\ny:100\n}",'utf8'))
-time.sleep(2)
-s.send(bytes("[new id]{to:1\nx:200\ny:200\n}",'utf8'))
+    create = "[new id]{new_id:" + str(id) + "\nx:" + str(x)+ "\ny:" + str(y) + "\n}"
 
-# close the connection
-s.close()
+
+    # receive data from the server and decoding to get the string.
+    s.send(bytes(create,'utf8'))
+    for i in range(0,4):
+        time.sleep(int(random() * 5))
+        x = int(random() * 1000)
+        y = int(random() * 1000)
+        move = "[new id]{to:" + str(id) + "\nx:" + str(x) + "\ny:" + str(y) + "\n}"
+        s.send(bytes(move,'utf8'))
+
+    # close the connection
+    s.close()
+
+threads = []
+for i in range(10):
+    thread = Thread(target = client_run,args = [i])
+    thread.start()
+    threads.append(thread)
+for i in range(10):
+    threads[i].join()
