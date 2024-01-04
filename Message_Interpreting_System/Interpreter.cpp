fe64f430d3132d0afa8b07bfa915d02c93351020
Tomer <tomer200000@gmail.com>
2024-01-04 15:42:33 +0300

    split interpreter to cpp and h files

---
+++ Message_Interpreting_System/Interpreter.cpp
@@ -0,0 +1,63 @@
+#include "Interpreter.h"
+
+Interpreter::Interpreter()
+{
+	alive_ = true;
+	system_state_ = system_state::RUNNING;
+}
+
+Interpreter::~Interpreter()
+{
+	close();
+}
+
+void Interpreter::run()
+{
+	std::unique_lock lock(alive_mutex_);
+
+	message::Message msg = incoming_messages_.stop_until_pop();
+	if (msg.direction == message::Message::message_direction::Out) {
+		mediator_->push_message(msg);
+	}
+	else {
+		message::Parsed_Message pmsg = message::parse_message(msg);
+		if (pmsg.type.find("Error") != -1) {
+			message::Message error_message = Message_Generator::generate_error_message(pmsg.type);
+			mediator_->push_message(error_message);
+			BOOST_LOG_TRIVIAL(warning) << "incoming message type not found,error message generated to be sent to client";
+		}
+		else
+		{
+			//TODO: add analysis of parsed message to determine where it should be pushed.
+			pmsg.destinations.emplace("representational network");
+			mediator_->push_parsed_message(pmsg);
+		}
+	}
+	if (alive_) {
+		lock.unlock();
+		run();
+	}
+	else
+		lock.unlock();
+}
+
+void Interpreter::provide_message(message::Message& msg)
+{
+	incoming_messages_.push(msg);
+}
+std::string Interpreter::component_name()
+{
+	return "interpreter";
+}
+void Interpreter::pause()
+{
+	system_state_ = system_state::PAUSED;
+	BOOST_LOG_TRIVIAL(info) << "Interpreter system paused";
+}
+
+void Interpreter::close()
+{
+	std::scoped_lock lock(alive_mutex_);
+	alive_ = false;
+	BOOST_LOG_TRIVIAL(info) << "Interpreter system closed";
+}
--- Message_Interpreting_System/Interpreter.h
+++ Message_Interpreting_System/Interpreter.h
@@ -49,67 +49,13 @@ Contact information:
 class Interpreter : public System<message::Message>, public Interface_Runnable
 {
 public:
-	Interpreter()
-	{
-		alive_ = true;
-		system_state_ = system_state::RUNNING;
-	}
-
-	~Interpreter()
-	{
-		close();
-	}
-
-	void run()
-	{
-		std::unique_lock lock(alive_mutex_);
-
-		message::Message msg = incoming_messages_.stop_until_pop();
-		if (msg.direction == message::Message::message_direction::Out) {
-			mediator_->push_message(msg);
-		}
-		else {
-			message::Parsed_Message pmsg = message::parse_message(msg);
-			if (pmsg.type.find("Error") != -1) {
-				message::Message error_message = Message_Generator::generate_error_message(pmsg.type);
-				mediator_->push_message(error_message);
-				BOOST_LOG_TRIVIAL(warning) << "incoming message type not found,error message generated to be sent to client";
-			}
-			else
-			{
-				//TODO: add analysis of parsed message to determine where it should be pushed.
-				pmsg.destinations.emplace("representational network");
-				mediator_->push_parsed_message(pmsg);
-			}
-		}
-		if (alive_) {
-			lock.unlock();
-			run();
-		}
-		else
-			lock.unlock();
-	}
-
-	virtual void provide_message(message::Message& msg)
-	{
-		incoming_messages_.push(msg);
-	}
-	std::string component_name()
-	{
-		return "interpreter";
-	}
-	void pause()
-	{
-		system_state_ = system_state::PAUSED;
-		BOOST_LOG_TRIVIAL(info) << "Interpreter system paused";
-	}
-
-	void close()
-	{
-		std::scoped_lock lock(alive_mutex_);
-		alive_ = false;
-		BOOST_LOG_TRIVIAL(info) << "Interpreter system closed";
-	}
+	Interpreter();
+	~Interpreter();
+	void run();
+	virtual void provide_message(message::Message& msg);
+	std::string component_name();
+	void pause();
+	void close();
 private:
 	bool alive_;
 	std::mutex alive_mutex_;
